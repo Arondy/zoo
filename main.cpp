60074e7c75ae47a4594fe9f68e38c65e0019731e
Arondy <antongatcenko@gmail.com>
2024-01-31 16:33:09 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using std::string, std::vector, std::map, std::pair, std::cout, std::cin, std::endl;
+
+enum Type {
+    open = 1, closed = 2, water = 3, alien = 4, alienWater = 5
+};
+
+class Animal {
+private:
+    string name;
+    int age;
+    int danger;
+public:
+    Animal(const string &name, int age, int danger) : name(name), age(age), danger(danger){}
+    Animal(const Animal &rhs) = default;
+    Animal(Animal &&rhs) noexcept = default;
+    Animal &operator=(const Animal &rhs) = default;
+    Animal &operator=(Animal &&rhs) noexcept = default;
+    ~Animal() = default;
+    const string &getName() const{
+        return name;
+    }
+    void setName(const string &name){
+        Animal::name = name;
+    }
+    int getAge() const{
+        return age;
+    }
+    void setAge(int age){
+        Animal::age = age;
+    }
+    int getDanger() const{
+        return danger;
+    }
+    void setDanger(int danger){
+        Animal::danger = danger;
+    }
+    bool operator==(const Animal &rhs) const{
+        return name == rhs.name &&
+               age == rhs.age &&
+               danger == rhs.danger;
+    }
+    bool operator!=(const Animal &rhs) const{
+        return !(rhs == *this);
+    }
+    bool operator<(const Animal &rhs) const{
+        return danger < rhs.danger;
+    }
+};
+
+class Cage {
+protected:
+    int id;
+    vector<Animal> animals;
+    float temp;
+    Type type;
+public:
+    Cage(int id, const vector<Animal> &animals, float temp, Type type) : id(id), animals(animals), temp(temp),
+                                                                         type(type){}
+    virtual ~Cage() = default;
+    int getId() const{
+        return id;
+    }
+    void setId(int id){
+        Cage::id = id;
+    }
+    const vector<Animal> &getAnimals() const{
+        return animals;
+    }
+    void setAnimals(const vector<Animal> &animals){
+        Cage::animals = animals;
+    }
+    float getTemp() const{
+        return temp;
+    }
+    void setTemp(float temp){
+        Cage::temp = temp;
+    }
+    Type getType() const{
+        return type;
+    }
+    void setType(Type type){
+        Cage::type = type;
+    }
+    void addAnimal(Animal &animal){
+        animals.push_back(animal);
+    }
+    void removeAnimal(const Animal &animal){
+        auto it = std::remove(animals.begin(), animals.end(), animal);
+        animals.erase(it, animals.end());
+    }
+    int getDanger(){
+        if (!animals.empty()){
+            std::make_heap(animals.begin(), animals.end());
+            return animals[0].getDanger() * type;
+        }
+    }
+};
+
+class OpenCage : public Cage {
+public:
+    OpenCage(int id, const vector<Animal> &animals, float temp) : Cage(id, animals, temp, open){}
+    ~OpenCage() override = default;
+    void operator<<(string str);
+};
+
+class ClosedCage : public Cage {
+public:
+    ClosedCage(int id, const vector<Animal> &animals, float temp) : Cage(id, animals, temp, closed){}
+    ~ClosedCage() override = default;
+    void switchLight() const{
+        cout << "You switched the light." << endl;
+    }
+//    friend void operator~(){
+//
+//    }
+};
+
+class WaterCage : public ClosedCage {
+protected:
+    float waterLevel;
+    map<string, float> wMap;
+public:
+    WaterCage(int id, const vector<Animal> &animals, float temp, float waterLevel, const map<string, float> &wMap) :
+            ClosedCage(id, animals, temp), waterLevel(waterLevel), wMap(wMap){ type = water; }
+    ~WaterCage() override = default;
+    float getWaterLevel() const{
+        return waterLevel;
+    }
+    void setWaterLevel(float waterLevel){
+        WaterCage::waterLevel = waterLevel;
+    }
+    const map<string, float> &getWMap() const{
+        return wMap;
+    }
+    void setWMap(const map<string, float> &wMap){
+        WaterCage::wMap = wMap;
+    }
+    WaterCage &operator+=(const pair<string, float> &pair){
+        wMap[pair.first] = pair.second;
+        return *this;
+    }
+};
+
+class AlienCage : public ClosedCage {
+protected:
+    map<string, float> aMap;
+public:
+    AlienCage(int id, const vector<Animal> &animals, float temp, const map<string, float> &aMap) :
+            ClosedCage(id, animals, temp), aMap(aMap){ type = alien; }
+    ~AlienCage() override = default;
+    const map<string, float> &getAMap() const{
+        return aMap;
+    }
+    void setAMap(const map<string, float> &aMap){
+        AlienCage::aMap = aMap;
+    }
+    AlienCage &operator+=(const pair<string, float> &pair){
+        aMap[pair.first] = pair.second;
+        return *this;
+    }
+};
+
+class AlienWaterCage : public WaterCage, public AlienCage {
+public:
+    AlienWaterCage(int id, const vector<Animal> &animals, float temp, float waterLevel,
+                   const map<string, float> &wMap, const map<string, float> &aMap) :
+                   WaterCage(id, animals, temp, waterLevel, wMap), AlienCage(id, animals, temp, aMap)
+                   { WaterCage::type = alienWater; }
+    ~AlienWaterCage() override = default;
+};
+
+int main(){
+    Animal a{"1", 3, 5};
+    vector<Animal> an{a};
+    OpenCage c{0, an, 45};
+    c.removeAnimal(a);
+    return 1;
+}
